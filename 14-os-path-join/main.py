0ee83889fbd0fc6b1209c8bee676df21455533c3
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-30 09:11:30 -0400

    14 - OS Path Join

---
+++ 14-os-path-join/main.py
@@ -0,0 +1,13 @@
+import os
+
+print("Path to a gif in documents:")
+path1 = os.path.join("Users", "SuperCoder64", "Documents", "reaction.gif")
+print(path1, "\n")
+
+print("Path to a secret banana bread recipe:")
+path2 = os.path.join("Secret", "DO NOT OPEN", "bbread.txt")
+print(path2, "\n")
+
+print("Path to a directory containing Python projects:")
+path3 = os.path.join("Users", "Reginald", "Documents", "Python", "Projects")
+print(path3)
---
+++ 14-os-path-join/readme.md
@@ -0,0 +1,5 @@
+# OS Path Join
+
+Briefly demonstrates using os.path.join() to create a string that
+represents a directory path that is valid for the operating system
+the script is run in.
