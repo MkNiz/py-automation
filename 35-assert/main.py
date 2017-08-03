8fd3a9aec389979d9cb997038ad80361a4a61ab9
Michael Nizzari <michael.nizzari@gmail.com>
2017-08-03 12:03:09 -0400

    35 - Assert

---
+++ 35-assert/main.py
@@ -0,0 +1,3 @@
+inp = input("Please do not press the red button.\n~> ")
+assert inp != "press red button", "WHY DID YOU PRESS THE RED BUTTON???"
+print("Thank you for not pressing the red button.")
---
+++ 35-assert/readme.md
@@ -0,0 +1,3 @@
+# Assert
+
+Demonstrates how assert can be used to raise an error when a condition is not met.
