9f6f634e04f725da512d9969f038a2f929576254
Michael Nizzari <michael.nizzari@gmail.com>
2017-08-19 09:36:45 -0400

    97 - pyautogui - Typewrite 2

--- 96-pyautogui-typewrite/main.py
+++ 96-pyautogui-typewrite/main.py
@@ -2,4 +2,5 @@ import pyautogui
 
 pyautogui.moveTo(380, 188, duration=0.5)
 pyautogui.click()
-pyautogui.typewrite("\nHey.\nI like to type stuff automatically because I'm\na robot.")
+pyautogui.typewrite(['h', 'e', 'l', 'o', 'left', 'l', 'right', 'enter', 'b', 'u', 'd'])
+
---
+++ 97-pyautogui-typewrite-2/readme.md
@@ -0,0 +1,4 @@
+# pyautogui - Typewrite 2
+
+Demonstrates sending a list of commands to typewrite, allowing key names like 'left'
+to be specified.
